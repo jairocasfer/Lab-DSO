dfc0d1f7a5c4830652374baaed44f413956d0393
jairocasfer <jairo.castelan@scitum.com.mx>
2025-04-15 18:46:24 -0400

    Version 2.6

--- terraform/ec2_instancia.tf
+++ terraform/ec2_instancia.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "webgoat_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ¡Restringe esto a tu IP en producción!
+    cidr_blocks = ["0.0.0.0/0"] # 
   }
 
   egress {
