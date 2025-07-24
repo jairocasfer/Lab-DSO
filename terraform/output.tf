1c2347dcd9ae1e59e8ee44762610830115601784
jairocasfer <jairo.castelan@scitum.com.mx>
2025-07-23 18:00:54 -0600

    version 3.2.4 ec2 borrar archivo output

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "webgoat_url" {
-  value = "http://${aws_instance.webgoat_server.public_ip}:8080/WebGoat"
-}
-
-output "webwolf_url" {
-  value = "http://${aws_instance.webgoat_server.public_ip}:9090"
-}
